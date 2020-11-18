de44a4bd828ce999c566e4fd61b3e9ad7eccba16
alexseveneight <alexseveneight@gmail.com>
2020-11-18 07:41:01 +0300

    'basics'

---
+++ src/components/search-panel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SearchPanel = () => {
+
+    const searchText = 'Type here!';
+    return <input placeholder={searchText} />
+  
+  }
+
+  export default SearchPanel
