18b9a7932ba7ec1804949b236a1fba56bc188a8c
alexseveneight <alexseveneight@gmail.com>
2020-11-22 12:48:12 +0300

    'array'

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppH from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
+import AppHeader from './components/app-header/app-header';
+import SearchPanel from './components/search-panel/search-panel';
+import TodoList from './components/todo-list/todo-list';
 
 const App = () => {
+
+  const todoData = [
+    { label: 'Drink Coffe', important: false },
+    { label: 'Make Awesome App', important: true },
+    { label: 'Buy Book', important: false }
+  ];
+  
+
   return (
     <div>
     <AppHeader />
     <SearchPanel />
-    <TodoList />
+    <TodoList todos={todoData}/>
   </div>
 
   )
