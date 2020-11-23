de086f8e9cedddc9c06d9f86b7cc0f0702f52343
alexseveneight <alexseveneight@gmail.com>
2020-11-23 14:00:33 +0300

    'deleting_item_without_changing_state'

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header/app-header';
 import SearchPanel from '../search-panel/search-panel';
@@ -7,30 +7,44 @@ import ItemStatusFilter from '../item-status-filter/item-status-filter';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    { label: 'Drink Coffe', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Buy Book', important: false, id: 3 }
-  ];
-  
+  state = {
+    todoData: [
+      { label: 'Drink Coffe', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Buy Book', important: false, id: 3 }
+    ]
+  }
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3}/>
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-      <TodoList 
-        todos={todoData}
-        onDeleted={(id) => console.log('del', id)}
-        />
-  </div>
+    deleteItem = (id) => {
+      this.setState(({todoData}) => {
+        const idx = todoData.findIndex((el) => el.id === id);
 
-  )
+        const newArr = [
+          ...todoData.slice(0, idx), 
+          ...todoData.slice(idx +1)
+        ]
+
+        return {
+          todoData: newArr
+        }
+    })}
+ 
+  render(){
+
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3}/>
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList 
+          todos={this.state.todoData}
+          onDeleted={this.deleteItem}
+          />
+    </div>
+    )
+  }
 }
-
-
-export default App;
